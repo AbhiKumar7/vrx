57b504961cd17569c41d151aa48d46a0df0a6def
Abhishek kumar <abhishekcodenotes@gmail.com>
2026-05-16 10:20:02 +0530

    added backend error logging

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -66,6 +66,7 @@ export const registerUser = async (req, res) => {
       accessToken,
     });
   } catch (error) {
+     console.log(error);
     return res.status(500).json({
       status: false,
       message: "Server error",
